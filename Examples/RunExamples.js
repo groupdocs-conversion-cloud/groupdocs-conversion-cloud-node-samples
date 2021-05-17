ca75a5949f91a6b2f4b8d9c9b9e353e0ef7b382f
Sergei Terentev <sergei.terentev@aspose.com>
2021-05-17 09:14:35 +0300

    Update examples for v21.4.1

--- Examples/RunExamples.js
+++ Examples/RunExamples.js
@@ -31,60 +31,63 @@ global.storageApi = conversion_cloud.StorageApi.fromConfig(config);
 
 async function examples() {
 
-    // Uploading sample test files to storage
-    await require('./Utils').UploadTestFiles();
+    try {        
+        // Uploading sample test files to storage
+        await require('./Utils').UploadTestFiles();
 
-    // Info API Examples
-    await require('./Info/GetSupportedConversions').Run();    
-    await require('./Info/GetDocumentInfo').Run();
+        // Info API Examples
+        await require('./Info/GetSupportedConversions').Run();    
+        await require('./Info/GetDocumentInfo').Run();
 
-    // Document conversion examples with conversion options
-    await require('./Convert/ConvertToHtml').Run();
-    await require('./Convert/ConvertToImage').Run();
-    await require('./Convert/ConvertToWordProcessing').Run();
-    await require('./Convert/ConvertToPdf').Run();
-    await require('./Convert/ConverToPdfResponseBody').Run();
-    await require('./Convert/ConvertToPresentation').Run();
-    await require('./Convert/ConvertToSpreadsheet').Run();
-    await require('./Convert/ConverToPdfDirect').Run();
+        // Document conversion examples with conversion options
+        await require('./Convert/ConvertToHtml').Run();
+        await require('./Convert/ConvertToImage').Run();
+        await require('./Convert/ConvertToWordProcessing').Run();
+        await require('./Convert/ConvertToPdf').Run();
+        await require('./Convert/ConverToPdfResponseBody').Run();
+        await require('./Convert/ConvertToPresentation').Run();
+        await require('./Convert/ConvertToSpreadsheet').Run();
+        await require('./Convert/ConverToPdfDirect').Run();
 
-    // Document conversion examples with common options
-    await require('./Common/AddWatermark').Run();
-    await require('./Common/ConvertConsecutivePages').Run();
-    await require('./Common/ConvertSpecificPages').Run();
-    await require('./Common/ConvertUsingCustomFont').Run();
+        // Document conversion examples with common options
+        await require('./Common/AddWatermark').Run();
+        await require('./Common/ConvertConsecutivePages').Run();
+        await require('./Common/ConvertSpecificPages').Run();
+        await require('./Common/ConvertUsingCustomFont').Run();
 
-    // Document conversion examples with loading options
-    await require('./LoadOptionsByDocumentType/Cad/ConvertCadAndSpecifyLoadOptions').Run();
-    await require('./LoadOptionsByDocumentType/Csv/ConvertCsvByConvertingDateTimeAndNumericData').Run();
-    await require('./LoadOptionsByDocumentType/Csv/ConvertCsvBySpecifyingDelimiter').Run();
-    await require('./LoadOptionsByDocumentType/Csv/ConvertCsvBySpecifyingEncoding').Run();
-    await require('./LoadOptionsByDocumentType/Email/ConvertEmailWithAlteringFieldsVisibility').Run();
-    await require('./LoadOptionsByDocumentType/Email/ConvertEmailWithAttachments').Run();
-    await require('./LoadOptionsByDocumentType/Email/ConvertEmailWithTimezoneOffset').Run();
-    await require('./LoadOptionsByDocumentType/Email/ConvertEmailWithFieldLabels').Run();
-    await require('./LoadOptionsByDocumentType/Email/ConvertEmailWithOriginalDate').Run();
-    await require('./LoadOptionsByDocumentType/Html/ConvertHtmlWithPageNumbering').Run();
-    await require('./LoadOptionsByDocumentType/Note/ConvertNoteBySpecifyingFontSubstitution').Run();
-    await require('./LoadOptionsByDocumentType/Pdf/ConvertPdfAndFlattenAllFields').Run();
-    await require('./LoadOptionsByDocumentType/Pdf/ConvertPdfAndHideAnnotations').Run();
-    await require('./LoadOptionsByDocumentType/Pdf/ConvertPdfAndRemoveEmbeddedFiles').Run();
-    await require('./LoadOptionsByDocumentType/Presentation/ConvertPresentationByHidingComments').Run();
-    await require('./LoadOptionsByDocumentType/Presentation/ConvertPresentationBySpecifyingFontSubstitution').Run();
-    await require('./LoadOptionsByDocumentType/Presentation/ConvertPresentationWithHiddenSlidesIncluded').Run();
-    await require('./LoadOptionsByDocumentType/Spreadsheet/ConvertSpreadsheetAndHideComments').Run();
-    await require('./LoadOptionsByDocumentType/Spreadsheet/ConvertSpreadsheetByShowingGridLines').Run();
-    await require('./LoadOptionsByDocumentType/Spreadsheet/ConvertSpreadsheetBySkippingEmptyRowsAndColumns').Run();
-    await require('./LoadOptionsByDocumentType/Spreadsheet/ConvertSpreadsheetBySpecifyingFontsubstitution').Run();
-    await require('./LoadOptionsByDocumentType/Spreadsheet/ConvertSpreadsheetBySpecifyingRange').Run();
-    await require('./LoadOptionsByDocumentType/Spreadsheet/ConvertSpreadsheetWithHiddenSheetsIncluded').Run();
-    await require('./LoadOptionsByDocumentType/Txt/ConvertTxtByControllingLeadingSpacesBehavior').Run();
-    await require('./LoadOptionsByDocumentType/Txt/ConvertTxtByControllingTrailingSpacesBehavior').Run();
-    await require('./LoadOptionsByDocumentType/Txt/ConvertTxtBySpecifyingEncoding').Run();
-    await require('./LoadOptionsByDocumentType/WordProcessing/ConvertWordProcessingByHidingComments').Run();
-    await require('./LoadOptionsByDocumentType/WordProcessing/ConvertWordProcessingByHidingTrackedChanges').Run();
-    await require('./LoadOptionsByDocumentType/WordProcessing/ConvertWordProcessingBySpecifyingFontSubstitution').Run();
-
+        // Document conversion examples with loading options
+        await require('./LoadOptionsByDocumentType/Cad/ConvertCadAndSpecifyLoadOptions').Run();
+        await require('./LoadOptionsByDocumentType/Csv/ConvertCsvByConvertingDateTimeAndNumericData').Run();
+        await require('./LoadOptionsByDocumentType/Csv/ConvertCsvBySpecifyingDelimiter').Run();
+        await require('./LoadOptionsByDocumentType/Csv/ConvertCsvBySpecifyingEncoding').Run();
+        await require('./LoadOptionsByDocumentType/Email/ConvertEmailWithAlteringFieldsVisibility').Run();
+        await require('./LoadOptionsByDocumentType/Email/ConvertEmailWithAttachments').Run();
+        await require('./LoadOptionsByDocumentType/Email/ConvertEmailWithTimezoneOffset').Run();
+        await require('./LoadOptionsByDocumentType/Email/ConvertEmailWithFieldLabels').Run();
+        await require('./LoadOptionsByDocumentType/Email/ConvertEmailWithOriginalDate').Run();
+        await require('./LoadOptionsByDocumentType/Html/ConvertHtmlWithPageNumbering').Run();
+        await require('./LoadOptionsByDocumentType/Note/ConvertNoteBySpecifyingFontSubstitution').Run();
+        await require('./LoadOptionsByDocumentType/Pdf/ConvertPdfAndFlattenAllFields').Run();
+        await require('./LoadOptionsByDocumentType/Pdf/ConvertPdfAndHideAnnotations').Run();
+        await require('./LoadOptionsByDocumentType/Pdf/ConvertPdfAndRemoveEmbeddedFiles').Run();
+        await require('./LoadOptionsByDocumentType/Presentation/ConvertPresentationByHidingComments').Run();
+        await require('./LoadOptionsByDocumentType/Presentation/ConvertPresentationBySpecifyingFontSubstitution').Run();
+        await require('./LoadOptionsByDocumentType/Presentation/ConvertPresentationWithHiddenSlidesIncluded').Run();
+        await require('./LoadOptionsByDocumentType/Spreadsheet/ConvertSpreadsheetAndHideComments').Run();
+        await require('./LoadOptionsByDocumentType/Spreadsheet/ConvertSpreadsheetByShowingGridLines').Run();
+        await require('./LoadOptionsByDocumentType/Spreadsheet/ConvertSpreadsheetBySkippingEmptyRowsAndColumns').Run();
+        await require('./LoadOptionsByDocumentType/Spreadsheet/ConvertSpreadsheetBySpecifyingFontsubstitution').Run();
+        await require('./LoadOptionsByDocumentType/Spreadsheet/ConvertSpreadsheetBySpecifyingRange').Run();
+        await require('./LoadOptionsByDocumentType/Spreadsheet/ConvertSpreadsheetWithHiddenSheetsIncluded').Run();
+        await require('./LoadOptionsByDocumentType/Txt/ConvertTxtByControllingLeadingSpacesBehavior').Run();
+        await require('./LoadOptionsByDocumentType/Txt/ConvertTxtByControllingTrailingSpacesBehavior').Run();
+        await require('./LoadOptionsByDocumentType/Txt/ConvertTxtBySpecifyingEncoding').Run();
+        await require('./LoadOptionsByDocumentType/WordProcessing/ConvertWordProcessingByHidingComments').Run();
+        await require('./LoadOptionsByDocumentType/WordProcessing/ConvertWordProcessingByHidingTrackedChanges').Run();
+        await require('./LoadOptionsByDocumentType/WordProcessing/ConvertWordProcessingBySpecifyingFontSubstitution').Run();
+    } catch (error) {
+        console.log(error);  
+    }
 }
 
 examples();
